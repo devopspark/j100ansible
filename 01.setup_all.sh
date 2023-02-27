4515d447b2865a08b62e742806785a8bfef91f04
devopspark <jeongeok.park@bespinglobal.com>
2023-02-27 16:43:22 +0900

    edited 01.setup_all.sh file

--- 01.setup_all.sh
+++ 01.setup_all.sh
@@ -13,6 +13,9 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
+# bridge taffic 보게 커널 파라메터 수정
+# 필요한 sysctl 파라미터를 /etc/sysctl.d/conf 파일에 설정하면, 재부팅 후에도 값이 유지된다.
+
 # Setup system parameters
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
@@ -20,9 +23,14 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
+# 재부팅하지 않고 sysctl 파라미터 적용하기
 sudo sysctl --system
 
-# Install packages in all nodes.
+# docker 엔진 설치
+sudo apt update
+sudo apt install -y docker.io 
+sudo systemctl enable --now docker
+
 # Install cri-docker
 sudo wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.0/cri-dockerd_0.3.0.3-0.ubuntu-bionic_amd64.deb
 sudo dpkg -i cri-dockerd_0.3.0.3-0.ubuntu-bionic_amd64.deb
