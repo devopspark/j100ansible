09802d0f4ab23559876f879766f5691e4179c897
devopspark <jeongeok.park@bespinglobal.com>
2023-02-28 10:56:02 +0900

    modified 01.setup_all.sh file

--- 01.setup_all.sh
+++ 01.setup_all.sh
@@ -4,7 +4,7 @@
 sudo sed -i s/'\#\$nrconf{kernelhints} = -1;'/'\$nrconf{kernelhints} = 0;'/g /etc/needrestart/needrestart.conf
 sudo sed -i s/'\#\$nrconf{restart} = \x27i\x27;'/'\$nrconf{restart} = \x27l\x27;'/g /etc/needrestart/needrestart.conf
 
-# Load modules(overlay, br_netfilter)
+# br_netfilter 모듈을 로드
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -31,25 +31,43 @@ sudo apt update
 sudo apt install -y docker.io 
 sudo systemctl enable --now docker
 
-# Install cri-docker
-sudo wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.0/cri-dockerd_0.3.0.3-0.ubuntu-bionic_amd64.deb
-sudo dpkg -i cri-dockerd_0.3.0.3-0.ubuntu-bionic_amd64.deb
-# ls /var/run/cri-dockerd.sock
+# cri-docker 설치
+sudo git clone https://github.com/Mirantis/cri-dockerd.git
 
-# Install kubeadm, kubelet, kubectl
-# Update the apt package index and install packages needed to use the Kubernetes apt repository:
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl
+mkdir bin
+VERSION=$((git describe --abbrev=0 --tags | sed -e 's/v//') || echo $(cat VERSION)-$(git log -1 --pretty='%h')) PRERELEASE=$(grep -q dev <<< "${VERSION}" && echo "pre" || echo "") REVISION=$(git log -1 --pretty='%h')
+go build -ldflags="-X github.com/Mirantis/cri-dockerd/version.Version='$VERSION}' -X github.com/Mirantis/cri-dockerd/version.PreRelease='$PRERELEASE' -X github.com/Mirantis/cri-dockerd/version.BuildTime='$BUILD_DATE' -X github.com/Mirantis/cri-dockerd/version.GitCommit='$REVISION'" -o cri-dockerd
+
+# Run these commands as root
+###Install GO###
+sudo wget https://storage.googleapis.com/golang/getgo/installer_linux
+sudo chmod +x ./installer_linux
+sudo ./installer_linux
+sudo source ~/.bash_profile
+
+sudo cd cri-dockerd
+sudo mkdir bin
+sudo go build -o bin/cri-dockerd
+sudo mkdir -p /usr/local/bin
+sudo install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+sudo cp -a packaging/systemd/* /etc/systemd/system
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# kubeadm, kubelet, kubectl 설치
+#Update the apt package index and install packages needed to use the Kubernetes apt repository:
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
 #Download the Google Cloud public signing key:
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg \
-    https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 #Add the Kubernetes apt repository:
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] \
-    https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
-sudo apt update
-sudo apt install -y kubelet=1.26.0-00 kubeadm=1.26.0-00 kubectl=1.26.0-00
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
